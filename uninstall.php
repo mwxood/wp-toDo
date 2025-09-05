e8c0a178237d3259da24950ee8a41d14f5c2de0d
Mihail <mwxood@users.noreply.github.com>
2025-09-05 11:01:25 +0300

    added uninstall option

--- uninstall.php
+++ uninstall.php
@@ -3,3 +3,12 @@
 if (!defined("ABSPATH")) {
     exit();
 }
+
+
+function wpToDo_uninstall()
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . "todo";
+    $wpdb->query("DROP TABLE IF EXISTS $table_name");
+}
+register_uninstall_hook(__FILE__, "wpToDo_uninstall");
